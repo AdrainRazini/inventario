87fa9d798a14bc423e5c250759f821a2efc99e8a
Adrian Razini Rangel <160901383+AdrainRazini@users.noreply.github.com>
2026-03-19 09:17:44 -0300

    // if

--- public/js/adicionar.js
+++ public/js/adicionar.js
@@ -526,7 +526,7 @@ document.getElementById('form-adicionar').onsubmit = async function (event) {
 
   try {
     const user = auth.currentUser;
-    if (!user) throw new Error("Usuário não autenticado.");
+    //if (!user) throw new Error("Usuário não autenticado.");
 
     // Você pode até buscar o doc do usuário para obter dados (ex: nome)
     // mas para criar não precisa verificar role
